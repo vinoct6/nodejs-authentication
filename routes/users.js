d5347217835eeb7d0dc15bf9f4db6d20d102c269
Vinoth Kumar <vinoth.mani2@wipro.com>
2018-09-26 13:21:18 +0530

    use bcrypt to hash passwords

--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,25 @@ const { User, userSchema, validateUser } = require('../models/user');
 const express = require('express');
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 router.post("/", async (request, response) => {
-    const userRequest = request.body;
-    const { error } = validateUser(userRequest);
+    const { error } = validateUser(request.body);
 
     if (error) {
         response.status(400).send(new Error(error));
         return;
     }
-    let user = new User(userRequest);
-        user = await user.save();
-   
 
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(request.body.password, salt);
+
+    let user = new User(_.pick(request.body, ['name', 'email']));
+    user.password = hashedPassword;
+
+    user = await user.save();
     response.send(_.pick(user, ['name', 'email']));
 });
 
 module.exports = router;
-
-
-
-
